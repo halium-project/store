f763e1d50ed2227e09ce84186630306bf860182e
Peltoche <dev@halium.fr>
2019-02-17 13:28:00 +0100

    Add a basic http server

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/halium-project/store
+
+require (
+	github.com/gorilla/mux v1.7.0 // indirect
+	github.com/halium-project/go-server-utils v0.0.0-20190217122131-a2b0df672213 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"context"
 
+	"github.com/gorilla/mux"
+	"github.com/halium-project/go-server-utils/endpoint"
+	"github.com/halium-project/go-server-utils/server"
+)
+
+const addr = ":42001"
+
+func main() {
+	ctx := context.Background()
+
+	router := mux.NewRouter()
+
+	// Expose utility endpoints.
+	router.HandleFunc("/ping", endpoint.Pinger).Methods("GET")
+
+	server.ServeHandler(router)
 }
