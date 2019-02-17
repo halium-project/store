98576c9cbf2cf0ebb9003a98cdd01f5c7ba62411
Peltoche <dev@halium.fr>
2019-02-17 13:33:49 +0100

    Fix the call to server.ServeHandler

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-
 	"github.com/gorilla/mux"
 	"github.com/halium-project/go-server-utils/endpoint"
 	"github.com/halium-project/go-server-utils/server"
@@ -11,12 +9,10 @@ import (
 const addr = ":42001"
 
 func main() {
-	ctx := context.Background()
-
 	router := mux.NewRouter()
 
 	// Expose utility endpoints.
 	router.HandleFunc("/ping", endpoint.Pinger).Methods("GET")
 
-	server.ServeHandler(router)
+	server.ServeHandler(addr, router)
 }
